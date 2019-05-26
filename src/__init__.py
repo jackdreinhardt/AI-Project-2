464b93fe5e9059a8a3f65111c681933976347c3d
Jack Reinhardt <reinh240@umn.edu>
2019-05-26 20:57:06 +0200

    init changes

--- src/__init__.py
+++ src/__init__.py
@@ -1,10 +1,2 @@
-import sys
 
-if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print("\nusage: python __init__.py \"Propositional Logic Statement\"\n")
-    else:
-        statement = sys.argv[1]
-
-    beliefs = deque()
     
