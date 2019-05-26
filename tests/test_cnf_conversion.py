013f17f080944fb2c2eabed73a12135f43e996d3
Jack Reinhardt <reinh240@umn.edu>
2019-05-27 01:35:16 +0200

    expanded tests

--- tests/test_cnf_conversion.py
+++ tests/test_cnf_conversion.py
@@ -2,6 +2,7 @@ import unittest
 from src.convert2CNF import convert2CNF
 from src.globals import *
 
+# Correct results of CNF conversion come from an online calculator
 class CNFConversionTests(unittest.TestCase):
     def setUp(self):
         self.c = convert2CNF()
@@ -17,13 +18,16 @@ class CNFConversionTests(unittest.TestCase):
     def test_implies_cnf(self):
         self.assertEqual(self.c.CNF(f"p{IMPLIES}q"), f"({NOT}p{OR}q)")
         self.assertEqual(self.c.CNF(f"(p{IMPLIES}q){IMPLIES}r"), f"((p{OR}r){AND}({NOT}q{OR}r))")
-        self.assertEqual(self.c.CNF(f"p{IMPLIES}q{IMPLIES}r"), f"({NOT}p{OR}({NOT}q{OR}r))")
-        self.assertEqual(self.c.CNF(f"p{IMPLIES}q{IMPLIES}r{IMPLIES}s"), f"({NOT}({NOT}({NOT}p{OR}q){OR}r){OR}s)")
+        self.assertEqual(self.c.CNF(f"p{IMPLIES}q{IMPLIES}r"), f"({NOT}p{OR}{NOT}q{OR}r)")
+        self.assertEqual(self.c.CNF(f"p{IMPLIES}q{IMPLIES}r{IMPLIES}s"), f"({NOT}p{OR}{NOT}q{OR}{NOT}r{OR}s)")
 
-    def test_biconditional(self):
-        self.assertEqual(self.c.CNF(f"p{BICONDITIONAL}q".format(BICONDITIONAL)), f"({NOT}p{OR}q){AND}({NOT}q{OR}p)")
-        self.assertEqual(self.c.CNF(f"p{BICONDITIONAL}q{BICONDITIONAL}r"), f"({NOT}(({NOT}p{OR}q){AND}({NOT}q{OR}p)){OR}r){AND}({NOT}r{OR}(({NOT}p{OR}q){AND}({NOT}q{OR}p)))")
+    def test_biconditional_cnf(self):
+        self.assertEqual(self.c.CNF(f"p{BICONDITIONAL}q"), f"({NOT}p{OR}q){AND}({NOT}q{OR}p)")
+        self.assertEqual(self.c.CNF(f"p{BICONDITIONAL}q{BICONDITIONAL}r"), f"(({NOT}p{OR}q){AND}(p{OR}{NOT}q){AND}({NOT}q{OR}r){AND}(q{OR}{NOT}r))")
 
+    def test_combinations_cnf(self):
+        self.assertEqual(self.c.CNF(f"(p{IMPLIES}q){BICONDITIONAL}(r{IMPLIES}s)"), f"(p{OR}{NOT}r{OR}s){AND}({NOT}q{OR}{NOT}r{OR}s)")
+        self.assertEqual(self.c.CNF(f"(({NOT}p){IMPLIES}(q{AND}r)){BICONDITIONAL}(r{IMPLIES}{NOT}(s{OR}(t{BICONDITIONAL}u)))"), f"({NOT}p{OR}{NOT}r{OR}{NOT}s){AND}({NOT}p{OR}{NOT}r{OR}t){AND}({NOT}p{OR}{NOT}r{OR}{NOT}u){AND}({NOT}q{OR}{NOT}r{OR}{NOT}s){AND}({NOT}q{OR}{NOT}r{OR}t){AND}({NOT}q{OR}{NOT}r{OR}{NOT}u)")
 
 if __name__ == '__main__':
     unittest.main()
