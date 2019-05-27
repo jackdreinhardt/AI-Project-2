07fd988f6aceada0b48c04ffce0b8a2af9c473ce
JannisHaber <47785203+JannisHaber@users.noreply.github.com>
2019-05-27 14:48:43 +0200

    Create mastermind.py

---
+++ mastermind/mastermind.py
@@ -0,0 +1,9 @@
+
+
+from src import belief_base, contract, revise, expand
+
+
+print("Choose from these colors: \n, red (r), green (g), yellow (y), blue (b)")
+
+code = str(input("Enter your secrect code: "))
+belief_base()
