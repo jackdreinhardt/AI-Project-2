905a3cc492706635114c04ed5020381dc1b9d662
Jack Reinhardt <reinh240@umn.edu>
2019-05-27 16:37:50 +0200

    finished module functions

--- src/logic_machine.py
+++ src/logic_machine.py
@@ -10,27 +10,24 @@ def cnf(s):
     IMPLIES = ->
     BICONDITIONAL = <->
 
-    Returns a string of propositional logic in Conjunctive Normal Form.
+    Returns a string of propositional logic in Conjunctive Normal Form (cnf).
     '''
     return convert2CNF.CNF(s)
 
 def belief_base(s):
     '''Construct a belief base from a propositional logic sentence
 
-    Takes in a string of propositional logic in Conjunctive Normal Form.
+    Takes in a string of propositional logic in cnf.
     Returns a BeliefBase object with s as the only belief
     '''
-    if convert2CNF.isCnf(s):
-        b = BeliefBase()
-        b.add_belief(Belief(s))
-        return b
-    else:
-        raise ValueError("input string must be in cnf form")
+    b = BeliefBase()
+    b.add_belief(Belief(s))
+    return b
 
 def entails(s, belief_base):
     '''Return true or false if s is entailed by the belief_base
 
-    Takes in a string of propositional logic in cnf form (s) and a BeliefBase object (belief_base)
+    Takes in a string s of propositional logic in cnf form and a BeliefBase object
     Returns true or false if the BeliefBase object entails s
     '''
     return belief_base.entails(Belief(s).to_string())
@@ -38,28 +35,30 @@ def entails(s, belief_base):
 def expand(s, belief_base):
     '''Expand the belief base to contain s
 
-    Takes in a string s of propositional logic and a BeliefBase object
+    Takes in a string s of propositional logic in cnf and a BeliefBase object
     Returns a BeliefBase object after expansion of s
 
     Note: Does not revise the belief base, so there may be contradictions
     '''
     belief_base.add_belief(Belief(s))
+    return belief_base
 
 def contract(s, belief_base):
     '''Contract the sentence s from belief_base using partial meet contraction
 
-    Takes in a string s of propositional logic and a BeliefBase object
+    Takes in a string s of propositional logic in cnf and a BeliefBase object
     Returns a BeliefBase object containing the input sentence in cnf form
     '''
-    belief_base.contract(cnf(s))
+    belief_base.contract(Belief(s))
     return belief_base
 
 def revise(s, belief_base):
     '''Add the sentence s to belief_base after contracting contradictions
 
-    Takes in a string s of propositional logic and a BeliefBase object
+    Takes in a string s of propositional logic in cnf and a BeliefBase object
     Returns a BeliefBase object that has been revised by s
     '''
+    belief_base.revise(Belief(s))
     return belief_base
 
 
