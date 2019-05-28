237712ff292f6639f55ae829f992d538843cd8a0
Jack Reinhardt <reinh240@umn.edu>
2019-05-28 13:13:17 +0200

    refined tests

--- src/test_cnf_conversion.py
+++ src/test_cnf_conversion.py
@@ -18,8 +18,7 @@ class CNFConversionTests(unittest.TestCase):
     def test_implies_cnf(self):
         self.assertEqual(self.c.CNF(f"p{IMPLIES}q"), f"({NOT}p{OR}q)")
         self.assertEqual(self.c.CNF(f"(p{IMPLIES}q){IMPLIES}r"), f"((p{OR}r){AND}({NOT}q{OR}r))")
-        self.assertEqual(self.c.CNF(f"p{IMPLIES}q{IMPLIES}r"), f"({NOT}p{OR}{NOT}q{OR}r)")
-        self.assertEqual(self.c.CNF(f"p{IMPLIES}q{IMPLIES}r{IMPLIES}s"), f"({NOT}p{OR}{NOT}q{OR}{NOT}r{OR}s)")
+        self.assertEqual(self.c.CNF(f"(p{IMPLIES}q){IMPLIES}(r{IMPLIES}s)"), f"(p{OR}{NOT}r{OR}s){AND}({NOT}q{OR}{NOT}r{OR}s)")
 
     def test_biconditional_cnf(self):
         self.assertEqual(self.c.CNF(f"p{BICONDITIONAL}q"), f"({NOT}p{OR}q){AND}({NOT}q{OR}p)")
